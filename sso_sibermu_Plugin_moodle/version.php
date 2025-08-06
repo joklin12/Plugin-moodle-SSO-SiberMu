249e098dd4c3e28b29951d780fc03d4c3416ceff
JokoVlog <148450286+joklin12@users.noreply.github.com>
2025-08-06 18:02:04 +0700

    Add files via upload

--- sso_sibermu_Plugin_moodle/version.php
+++ sso_sibermu_Plugin_moodle/version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'auth_sso_sibermu';
-$plugin->version   = 2024073001; // YYYYMMDDXX (Date of creation + revision number).
+$plugin->version   = 2024073003; // YYYYMMDDXX (Date of creation + revision number).
 $plugin->requires  = 2018051700; // Moodle 3.5+.
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '1.0.1';
